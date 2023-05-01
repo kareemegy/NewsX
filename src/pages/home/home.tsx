867e11eac42cfa8a9536af708684a97c1fc561a9
kareemegy <kokekarim@yahoo.com>
2023-05-01 21:12:27 +0300

    add svg to ui

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -14,11 +14,26 @@ const Home = () => {
         </Button>
       </nav>
       <main className="container mx-auto px-4 mt-[40px] flex justify-between h-[600px]">
-        <div className="flex flex-col justify-center w-[500px]">
+        <div className="flex flex-col justify-center w-[500px] ">
           <h1 className="text-6xl mb-8 font-bold">
             Your Source for{" "}
             <span className=" text-blue">Fast and Reliable</span> News Coverage
           </h1>
+          <div className="relative top-0">
+            <svg
+              className="absolute w-72 md:w-96 -right-24 -top-[250px]"
+              width="395"
+              height="249"
+              viewBox="0 0 395 249"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M198.861 231.832C90.5984 199.942 -65.8655 63.6109 29.654 13.4247C69.206 -7.35606 99.4642 0.847046 143.831 6.12572C261.437 20.1182 366.758 133.203 390.716 182.237C414.675 231.271 334.189 271.696 198.861 231.832Z"
+                fill="#697CE3"
+                fill-opacity="0.1"
+              ></path>
+            </svg>
+          </div>
           <p className="text-xl">
             The NEWSX allows users to search for articles from a vast range of
             sources, including over 30,000 news sources and blogs. Users can
