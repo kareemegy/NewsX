cf45b8fc376c971f581f52ba2fa556943654b697
kareemegy <kokekarim@yahoo.com>
2023-05-04 18:53:01 +0300

    add firebase to log ing page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,25 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { signIn } from "../../lib/Firebase/Firebase";
 import Button from "../../components/button/button";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const handleEmailInput = (event: any) => {
+    setEmail(event.target.value);
+  };
+  const handlePasswordInput = (event: any) => {
+    setPassword(event.target.value);
+  };
+  const handleSignUp = async () => {
+    const user = await signIn(email, password);
+    setEmail("");
+    setPassword("");
+    navigate("/dashboard");
+    console.log(user);
+  };
   return (
     <>
       <div className="container m-auto p-4 h-[100vh] flex flex-col items-center justify-center">
@@ -11,20 +29,28 @@ const Login = () => {
           alt="News LOGO"
         />
         <h1 className="text-3xl mb-3">Login</h1>
+
         <input
           className="inline-block bg-slate-200 m-1 p-3 rounded"
           placeholder="Email"
           type="email"
           name="email"
+          value={email}
+          onChange={handleEmailInput}
         />
         <input
           className="inline-block bg-slate-200 my-3 p-3 rounded"
           placeholder="password"
           type="password"
           name="password"
+          value={password}
+          onChange={handlePasswordInput}
         />
-        <Button className="bg-blue px-8 py-3 rounded-md text-xl text-white mb-6">
-          LOG IN
+        <Button
+          handleClick={handleSignUp}
+          className="bg-blue px-8 py-3 rounded-md text-xl text-white mb-6"
+        >
+          Sign In
         </Button>
         <Link to="/register"> Don't have an account? Sign up</Link>
       </div>
